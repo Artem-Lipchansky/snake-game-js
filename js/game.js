158b1ae4c5c827d802c73473fb5e077a065b8db2
Artem Lipchansky <lipchanskiy.an@gmail.com>
2023-05-26 11:46:15 +0300

    added

--- js/game.js
+++ js/game.js
@@ -3,3 +3,17 @@ const ctx = canvas.getContext("2d");
 
 const ground = new Image();
 ground.src = "img/ground.png";
+
+const food = new Image();
+food.src = "img/food.png";
+
+
+let box = 32;
+
+let score = 0;
+
+function drawGame() {
+    ctx.drawImage(ground, 0, 0);
+}
+
+let game = setInterval(drawGame, 100);
